386b571d94b0c0fcb4ff203a1d028c74d55aec21
Amund Tenstad <github@amund.io>
2018-06-12 16:20:42 +0200

    Implement add/remove nodes/edges

--- Action.elm
+++ Action.elm
@@ -3,6 +3,7 @@ module Action exposing (..)
 import Model exposing (..)
 import Depth exposing (..)
 
+
 addNodes : Model -> List ( Int, String ) -> Model
 addNodes model nodes =
     case nodes of
@@ -11,15 +12,43 @@ addNodes model nodes =
                 addNodes { model | nodes = { id = id, name = name } :: model.nodes } rest
             else
                 addNodes model rest
+
         _ ->
             { model | depthNodes = calculateDepth model.edges model.nodes }
+
+
+removeNodes : Model -> List Int -> Model
+removeNodes model nodes =
+    let
+        newNodes =
+            List.filter (\n -> not (List.member n.id nodes)) model.nodes
+
+        newEdges =
+            List.filter (\e -> (not (List.member e.from nodes)) && (not (List.member e.to nodes))) model.edges
+    in
+        { model | nodes = newNodes, edges = newEdges, depthNodes = calculateDepth newEdges newNodes }
 
 
 addEdges : Model -> List ( Int, Int ) -> Model
 addEdges model edges =
     case edges of
         ( from, to ) :: rest ->
-            addEdges { model | edges = { from = from, to = to } :: model.edges } rest
+            let
+                edge =
+                    { from = from, to = to }
+            in
+                if not (List.member edge model.edges) then
+                    addEdges { model | edges = { from = from, to = to } :: model.edges } rest
+                else
+                    addEdges model rest
 
         _ ->
             { model | depthNodes = calculateDepth model.edges model.nodes }
+
+
+removeEdges : Model -> List ( Int, Int ) -> Model
+removeEdges model edges =
+    let
+        newEdges = List.filter (\e -> not (List.member (e.from, e.to) edges)) model.edges
+    in
+        { model | edges = newEdges, depthNodes = calculateDepth newEdges model.nodes }
--- Communication.elm
+++ Communication.elm
@@ -9,25 +9,49 @@ handleCommunication : Model -> String -> ( Model, Cmd Msg )
 handleCommunication model msg =
     case decodeString (maybe (field "command" string)) msg of
         Ok (Just command) ->
-            case decodeString (maybe (field "nodes" (keyValuePairs string))) msg of
-                Ok (Just rawNodes) ->
-                    let
-                        nodes =
-                            rawNodes
-                                |> List.filterMap
-                                    (\( k, v ) ->
-                                        case ( String.toInt k ) of
-                                            Ok id ->
-                                                Just (id, v)
+            case command of
+                "addNodes" ->
+                    case decodeString (maybe (field "nodes" (keyValuePairs string))) msg of
+                        Ok (Just rawNodes) ->
+                            ( addNodes model (List.filterMap validateIntStringPair rawNodes), Cmd.none )
 
-                                            _ ->
-                                                Nothing
-                                    )
-                    in
-                        ( addNodes model nodes, Cmd.none )
+                        _ ->
+                            ( model, Cmd.none )
+
+                "removeNodes" ->
+                    case decodeString (maybe (field "nodes" (list int))) msg of
+                        Ok (Just nodes) ->
+                            ( removeNodes model nodes, Cmd.none )
+
+                        _ ->
+                            ( model, Cmd.none )
+
+                "addEdges" ->
+                    case decodeString (maybe (field "edges" (list (map2 (,) (index 0 int) (index 1 int))))) msg of
+                        Ok (Just edges) ->
+                            ( addEdges model edges , Cmd.none )
+                        _ ->
+                            ( model, Cmd.none )
+
+                "removeEdges" ->
+                    case decodeString (maybe (field "edges" (list (map2 (,) (index 0 int) (index 1 int))))) msg of
+                        Ok (Just edges) ->
+                            ( removeEdges model edges , Cmd.none )
+                        _ ->
+                            ( model, Cmd.none )
 
                 _ ->
                     ( model, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
+
+
+validateIntStringPair : ( String, String ) -> Maybe ( Int, String )
+validateIntStringPair ( int, str ) =
+    case (String.toInt int) of
+        Ok int ->
+            Just ( int, str )
+
+        _ ->
+            Nothing
