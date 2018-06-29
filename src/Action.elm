db9576a3429006349bf14d5233789a2eb95a1b97
Amund Tenstad <github@amund.io>
2018-06-29 15:08:09 +0200

    Auto position

--- src/Action.elm
+++ src/Action.elm
@@ -352,6 +352,49 @@ layout model =
 xyLayout : List String -> Model -> Model
 xyLayout layout ({ nodes, edges } as model) =
     let
+        minX =
+            nodes
+                |> List.map .x
+                |> List.filterMap identity
+                |> List.minimum
+                |> Maybe.withDefault 0
+
+        minY =
+            nodes
+                |> List.map .y
+                |> List.filterMap identity
+                |> List.minimum
+                |> Maybe.withDefault 0
+
+        positions =
+            List.filterMap
+                (\n ->
+                    case ( n.x, n.y ) of
+                        ( Just x, Just y ) ->
+                            Just ( x, y )
+
+                        _ ->
+                            Nothing
+                )
+                nodes
+
+        minDistnace =
+            positions
+                |> List.concatMap
+                    (\( x1, y1 ) ->
+                        List.filterMap
+                            (\( x2, y2 ) ->
+                                if x1 == x2 && y1 == y2 then
+                                    Nothing
+                                else
+                                    Just <| (x2 - x1) ^ 2 + (y2 - y1) ^ 2
+                            )
+                            positions
+                    )
+                |> List.minimum
+                |> Maybe.withDefault 1
+                |> (\n -> n ^ 0.5)
+
         newNodes =
             List.map
                 (\n ->
@@ -359,7 +402,7 @@ xyLayout layout ({ nodes, edges } as model) =
                         | position =
                             case ( n.x, n.y ) of
                                 ( Just x, Just y ) ->
-                                    Just { x = x, y = y }
+                                    Just { x = (x - minX) / minDistnace, y = (y - minY) / minDistnace }
 
                                 _ ->
                                     Nothing
