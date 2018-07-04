f4dbc5542c8594e91b7dd88f0c2642f7b9c216cb
Amund Tenstad <github@amund.io>
2018-07-04 15:02:08 +0200

    Improve tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,33 +1,55 @@
-var nodes = [];
-var apps = [];
+class Test {
+    constructor(node, app, name) {
+        this.node = node;
+        this.app = app;
+        this.name = name;
+        this.lastInput = {};
+    }
+
+    resize() {
+        this.app.ports.input.send(JSON.stringify(Object.assign({}, this.defaults(), this.lastInput)));
+    }
+
+    send(input) {
+        this.lastInput = input;
+        this.app.ports.input.send(JSON.stringify(Object.assign({}, this.defaults(), input)));
+    }
+
+    defaults() {
+        return {
+            "name": this.name,
+            "size": [this.node.offsetWidth, this.node.offsetHeight],
+            "position": [this.node.offsetLeft, this.node.offsetTop]
+        };
+    }
+}
 
 function createTest(name) {
+    var codename = name.replace(new RegExp(' ', 'g'), '');
     var node = document.createElement('div');
     node.classList.add('test');
-    node.id = name;
-    nodes.push(node);
+    node.id = codename;
 
-    var app = Elm.Main.embed(node, {'webSocket': null});
-    apps.push(app);
+    var app = Elm.Main.embed(node, {'webSocket': null, 'disableWindowResize': true});
+    var test = new Test(node, app, codename);
 
     var title = document.createElement('p');
     title.innerHTML = name;
 
     node.appendChild(title);
-    document.body.appendChild(node);
+    document.getElementById('tests').appendChild(node);
 
-    var codename = name.replace(new RegExp(' ', 'g'), '');
+    node.addEventListener('mousewheel', function (e) {
+        e.preventDefault();
+    });
+
+    window.addEventListener('resize', function(event){
+        test.resize();
+    });
 
     return new Promise(function (callback) {
         setTimeout(function() {
-            callback(function(input) {
-                defaults = {
-                    "name": codename,
-                    "size": [node.offsetWidth, node.offsetHeight],
-                    "position": [node.offsetLeft, node.offsetTop]
-                };
-                app.ports.input.send(JSON.stringify(Object.assign({}, defaults, input)));
-            });
+            callback(function(input) {test.send(input)});
         }, 100);
     });
 }
