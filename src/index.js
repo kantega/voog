f336611dbb45afb043c63c27adef250d42f839d6
Amund Tenstad <github@amund.io>
2018-12-10 00:16:08 +0100

    Update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import { Voog } from "./voog"
+import {Voog} from "./voog"
 
 const voog = Voog.init('main');
 
-voog.send({setEdges: [{from: 0, to: 1}]});
+voog.send(
+    {
+        setNodes: [
+            {id: 0, name: "Hello"},
+            {id: 1, name: "World!"}
+        ],
+        setEdges: [
+            {from: 0, to: 1}
+        ],
+    }
+);
+
+setInterval(function () {
+    voog.send({
+        'addMovement': [{'from': 0, 'to': 1, 'duration': 1, 'icon': 'dot'}]
+    });
+}, 500);
