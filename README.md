0b91f8d4b53b2f4e71b1b86608b84cd22dd0b71f
Amund Tenstad <github@amund.io>
2018-12-10 16:00:31 +0100

    Add code example

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ![voog](https://cdn.rawgit.com/kantega/voog/gh-pages/assets/img/logo.svg)
 
+## Getting started
+### With npm
+`npm install --save @kantega/voog`
+```javascript
+import {Voog} from "@kantega/voog";
+const voog = Voog.init('main');
+voog.send({ .. });
+```
+
+### Pure browser
+```html
+<html>
+  <body>
+      <div id="main"></div>
+  </body>
+
+  <script src="voog.js"></script>
+  <script>
+    const voog = Voog.init('main');
+    voog.send({ .. });
+  </script>
+</html>
+```
+
 Read more on our [GitHub page](https://kantega.github.io/voog)
--- src/index.js
+++ src/index.js
@@ -4,27 +4,20 @@ import {Voog} from "./voog";
 
 const voog = Voog.init('main');
 
-voog.send(
-    {
-        setNodes: [
-            {id: 0, name: "Hello"},
-            {id: 1, name: "World!"},
-        ],
-        setEdges: [
-            {from: 0, to: 1}
-        ],
-    }
-);
+voog.send({
+    setNodes: [
+        {id: 0, name: "Hello"},
+        {id: 1, name: "World!"},
+    ],
+    setEdges: [
+        {from: 0, to: 1},
+    ],
+});
 
-setInterval(function () {
+setInterval(() => {
     voog.send({
-        'addMovement': [
-            {
-                'from': 0,
-                'to': 1,
-                'duration': 1,
-                'icon': 'dot',
-            }
+        addMovement: [
+            {from: 0, to: 1, duration: 1, icon: 'dot'},
         ]
     });
 }, 500);
